6ad57dde380011b7f97a69646760a159ff0a9cf5
Daniel-cpi <danieloutlaws55@gmail.com>
2024-12-24 21:39:55 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "samp-hosting",
+  "version": "1.0.0",
+  "description": "SA-MP hosting dashboard",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
